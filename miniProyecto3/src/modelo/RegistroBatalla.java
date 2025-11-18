df9355a9d3c477f58e9cd914a646849ab04be9ff
Andres770-Q <quiceno.andres@correounivalle.edu.co>
2025-11-17 23:48:31 -0500

    Se corrigio el bug acerca del congelamiento de la interfaz con un SwingWorked que es un hilo diferente al hilo de swing para en este caso hacer la tarea pesada de correr la logica la cual es muy densa.Se termino de corregir el bug de las habilidades con el Heroe.Tambien se le añadio bloqueos y desbloqueos a los botones para orientar el usuario.Finalmente cuando se acaba la partidad se desbloquea el boton de volver a jugar para arrancar una partida nueva

--- miniProyecto3/src/modelo/RegistroBatalla.java
+++ miniProyecto3/src/modelo/RegistroBatalla.java
@@ -1,6 +1,7 @@
 package modelo;
 
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 public class RegistroBatalla { //Clase Creada como un intermediario que me permite registrar lo que sucede en tiempo real al JTextArea
     private static JTextArea area;
@@ -10,11 +11,12 @@ public class RegistroBatalla { //Clase Creada como un intermediario que me permi
     }
         //Metodo estatico que no tiene la necesidad de ser instansiado es decir se puede utilizar en cualquier clase normal
     public static void RegistrarTextos(String mensaje) {//Registra en el JTextArea Las Acciones que van sucediendo en la partida
-        if (area != null) {
-            area.append(mensaje + "\n");
-        } else {
-            System.out.println(mensaje); // Respaldo si no registra en el JTextArea
-        }
+            if (area != null) {
+                SwingUtilities.invokeLater(() -> { //Para que actualize correctamente el texto a pesar de manejarse otro hilo aparte para la logica pesada
+                 area.append(mensaje + "\n");
+                 });
+            }
     }
 
 }
+
